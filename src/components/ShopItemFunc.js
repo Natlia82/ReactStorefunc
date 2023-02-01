0e81bef44b838492f42b3e55c5b806d39874f3f6
Natalia <nata@host>
2023-02-01 17:01:21 +0500

    готово!

--- src/components/ShopItemFunc.js
+++ src/components/ShopItemFunc.js
@@ -5,26 +5,34 @@ import PropTypes from "prop-types";
 function ShopItemFunc(props) {
     const {item} = props;
 
-    return <div class="main-content">
+    return <div className="main-content">
                 <h2>{item.brand}</h2>
                 <h1>{item.title}</h1>
                 <h3>{item.description}</h3>
-                <div class="description">
+                <div className="description">
                     {item.descriptionFull}
                 </div>
-                <div class="highlight-window mobile">
-                    <div class="highlight-overlay"></div>
+                <div className="highlight-window mobile">
+                    <div className="highlight-overlay"></div>
                 </div>
-                <div class="divider"></div>
-                <div class="purchase-info">
-                    <div class="price">{item.currency}{item.price}</div>
+                <div className="divider"></div>
+                <div className="purchase-info">
+                    <div className="price">{item.currency}{item.price}</div>
                     <button>Добавить в корзину</button>
                 </div>
             </div>
 }
 
 ShopItemFunc.prototype = {
-    item: PropTypes.instanceOf(ItemModel).isRequired
+  /*  item: PropTypes.instanceOf(ItemModel).isRequired();*/
+  item: PropTypes.shape({
+    brand: PropTypes.string,
+    title: PropTypes.string,
+    description: PropTypes.string,
+    descriptionFull: PropTypes.string,
+    price: PropTypes.number,
+    currency: PropTypes.string,
+  })
 }
 
 export default ShopItemFunc;
